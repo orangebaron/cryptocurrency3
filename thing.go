87dfe77df3f8fac1b65112b58b24db3aadd5d147
Orange Baron <orangebaronroblox@gmail.com>
2017-07-19 19:25:56 -0400

    MM

--- thing.go
+++ thing.go
@@ -139,8 +139,8 @@ func main() {
 
 	nodelist = os.Args[1:]
 	if len(nodelist) == 0 {
-		addToChain([]block{"a"})
-		addToChain([]block{"b"})
+		addToChain([]byte{"a"})
+		addToChain([]byte{"b"})
 	}
 
 	//get lengths
